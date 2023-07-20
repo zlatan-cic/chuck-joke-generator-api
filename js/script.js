1d3fd2b8b989b5609978e7d18aa05ee3485ae00a
zlatan-cic <smajicz999@gmail.com>
2023-07-20 20:53:19 +0200

    HTML and CSS.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('btn-joke');
+
+console.log(jokeBtn);
+console.log(jokeEl);
+
+jokeBtn.addEventListener('click', function(){
+  console.log("I have been click");
+})
